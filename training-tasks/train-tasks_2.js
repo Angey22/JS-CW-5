6b7e229a0ee380b71903faa44a1fbfe15c74f2ed
Angey22 <babandval@gmail.com>
2023-04-22 21:31:28 +0300

    Added link in file 'index.html' & folder 'training-tasks' in project

---
+++ training-tasks/train-tasks_2.js
@@ -0,0 +1,7 @@
+// Task X
+// console.groupCollapsed('Task X'); //! Log group name
+//! Task conditions
+//! Task solution
+//! Check test
+// console.groupEnd(); //! Log group end
+// console.log('-X----------------------------------------------');
